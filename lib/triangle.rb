cf355a8d21798fd2fec97a809771ae10940be5b4
Max Mezalon <jmezalon@gmail.com>
2021-11-25 21:55:49 -0500

    commit tree 208d381090617518d961a2055135694521430d2d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+
+    valid_triangles
+      if @a == @b && @a == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      else
+        :scalene
+      end
+  end
+
+  def sides_are_positive?
+    [@a, @b, @c].all?(&:positive?)
+  end
+
+  def sides_iniquality?
+    @a + @b > @c && @a + @c > @b && @b + @c > @a
+  end
+
+  def valid_triangles
+    raise TriangleError unless sides_are_positive? && sides_iniquality?  
+  end
+
+  class TriangleError < StandardError
+  end
 end
